ab8827cbc7f327a90b2d0c96acf5c3a1699b8ff8
Serkan OZCAN <serkan@Serkan-MacBook-Pro.local>
2017-07-15 20:42:39 +0300

    -

--- example.php
+++ example.php
@@ -1,18 +1,21 @@
 <?php
-
 include './vendor/autoload.php';
 
 use \TempMailAPI\TempMail;
 
+$username = "cigdem_amidar";
+$dmain = "";
+
 $tempMail = new TempMail();
 // $response = $tempMail->getDomains();
 
-// $response = $tempMail->getNewAddress("new_mail_addr", "@domain.com");
-
-$response = $tempMail->getNewAddress("yesim_ali_1998", "@vektiks.com");
+$response = $tempMail->getNewAddress("yesim_ali_1998", "@vektik.com");
 var_dump($response);
 // $mail = $response;
 
+$response = $tempMail->getMails("yesim_ali_1998@vektik.com");
+var_dump($response);
 
-// $response = $tempMail->getMails("canisi_kiz_ist_89@vektik.com");
+
+// $response = $tempMail->readMail("canisi_kiz_ist_89@vektik.com");
 // var_dump($response);
