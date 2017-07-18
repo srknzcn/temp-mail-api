7e0220823f5e6438986f533249b3ec302b4373cf
Serkan OZCAN <serkan@Serkan-MacBook-Pro.local>
2017-07-18 17:49:27 +0300

    -

--- example.php
+++ example.php
@@ -16,13 +16,13 @@ $tempMail = new TempMail();
 // var_dump($response);
 // $mail = $response;
 
-// $response = $tempMail->getMails("yesim_ali_1998@vektik.com", "insta");
-// var_dump($response);
+$response = $tempMail->getMails("xorohutet@vektik.com", "Insta");
+var_dump($response);
 
 
 // $response = $tempMail->readMail("https://temp-mail.org/en/view/de556c8b00a3f68011b52c507c0c10c6/");
 // $response = $tempMail->readMail("https://temp-mail.org/en/view/2e21ae9325a9e93c31e1948f68bdc794");
 // var_dump($response);
 
-$data = file_get_contents("./test.txt");
-$instagram = new InstagramAccountConfirmationHelper($data);
+// $data = file_get_contents("./test.txt");
+// $instagram = new InstagramAccountConfirmationHelper($data);
